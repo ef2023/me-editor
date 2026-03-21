a14dff972d6f3decdf57fbd3daad03fe8e4f405e
ef2023 <e.f2023@hotmail.com>
2026-03-21 03:02:55 -0300

    remove port

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -5,7 +5,6 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'cdn.sanity.io',
-        port: 'http://localhost:3000',
         pathname: '/images/**',
       },
     ],

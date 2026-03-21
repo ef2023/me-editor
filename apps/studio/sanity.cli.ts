027ef728c064db11b421871c367671f9ff472b9c
ef2023 <e.f2023@hotmail.com>
2026-03-21 01:31:48 -0300

    add id studio

--- apps/studio/sanity.cli.ts
+++ apps/studio/sanity.cli.ts
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli';
 
 export default defineCliConfig({
   api: {
-    projectId: process.env.SANITY_STUDIO_PROJECT_ID || '',
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID || 'g6fc3waj',
     dataset: process.env.SANITY_STUDIO_DATASET || 'production',
   },
 });
